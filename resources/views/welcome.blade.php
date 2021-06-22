1dfb67b69ac2a77956eeeb7aec05ad900cdaf94f
yos <adikagrafityler@gmail.com>
2021-06-22 11:29:07 +0700

    commit kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Simagi
                 </div>
 
                 <div class="links">
