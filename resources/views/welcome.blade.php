1b4e1dfef2684e1f625a2e0dec82370b8ecc7d8f
yos <adikagrafityler@gmail.com>
2021-06-22 12:24:32 +0700

    1.0.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Simagilagii
+                    Simagi v.1
                 </div>
 
                 <div class="links">
